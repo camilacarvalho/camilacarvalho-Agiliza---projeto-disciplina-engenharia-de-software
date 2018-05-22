87d001a82075b25d68c7f5f3155e74c6d4b44f60
PedroGuedesBraga <pedro.braga@ccc.ufcg.edu.br>
2018-05-22 19:04:21 -0300

    Registra usuario no firestore quando o mesmo loga pela primeira vez

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,23 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp()
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
- export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+
+     export const createUser = functions.auth.user().onCreate(event => {
+    const user = {
+        name: event.displayName,
+        firstName: "",
+        lastName: "",
+        photo: event.photoURL,
+        role: "",
+        city: "",
+        state: "",
+        country: "",
+        bio: ""  
+    }
+    return admin.firestore().collection('users').doc(event.uid).set(user);
+ }); 
+
+    export const helloWorld = functions.https.onRequest((request, response) => {
+        response.send("teste");
+    });
