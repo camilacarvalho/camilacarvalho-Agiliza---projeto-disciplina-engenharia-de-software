96ba09e8b780b3f4a81e8b12b0804de071ad5331
camilacarvalho <camila.silva@ccc.ufcg.edu.br>
2018-06-08 13:14:01 -0300

    Implementa página do perfil do usuário

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -12,7 +12,7 @@ import { PerfilUsuarioPage } from '../pages/perfil-usuario/perfil-usuario';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any =  PerfilUsuarioPage;
+  rootPage:any =  LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
