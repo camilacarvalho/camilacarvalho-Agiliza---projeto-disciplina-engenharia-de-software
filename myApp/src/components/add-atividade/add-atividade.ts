e6a5ba9297e524621ed326563565645ea4daf75e
camilacarvalho <camila.silva@ccc.ufcg.edu.br>
2018-06-17 13:55:25 -0300

    designer tela adicionar atividade

--- myApp/src/components/add-atividade/add-atividade.ts
+++ myApp/src/components/add-atividade/add-atividade.ts
@@ -1,18 +1,42 @@
 import { Component } from '@angular/core';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'add-atividade',
   templateUrl: 'add-atividade.html'
 })
 export class AddAtividadeComponent {
+
   private atividade: any;
   private data_atual;
-  constructor() {
-    this.atividade={nome:"",prazo:"",agenda:false,colaboradores:[] }
+  private colaboradores: string[];
+  constructor(public toastCtrl: ToastController) {
+    this.initializeColaboradores();
+    this.atividade={nome:"",prazo:"",agenda:false,colaboradores_atv:[] }
   }
   criarAtividade(){
-    console.log(this.atividade);
+    if(this.camposValidos()==true){
+      this.presentToast("Alterações salvas!");
+    }
   }
+  initializeColaboradores(): any {
+    this.colaboradores = ["camila.silva@ccc.ufcg.edu.br","samara.sampaio@ccc.ufcg.edu.br","pedro.braga@ccc.ufcg.edu.br","anderson.vital@ccc.ufcg.edu.br","aramis.araujo@ccc.ufcg.edu.br", "jose.ivan.junior@ccc.ufcg.edu.br", "gabriel.alves@ccc.ufcg.edu.br", "bruno.silva@ccc.ufcg.edu.br", "lucas.christopher.silva@ccc.ufcg.edu.br", "sheilla.silva@ccc.ufcg.edu.br"];
+  }
+  presentToast(mensagem) {
+    let toast = this.toastCtrl.create({
+      message: mensagem,
+      duration: 3000
+    });
+    toast.present();
+  }
+  camposValidos() {
+    if ((this.atividade.nome.trim() == "") || (this.atividade.prazo.trim() == "") || (this.atividade.colaboradores_atv.length==0)) {
+      this.presentToast("Preencha todos os campos!");
+      return false;
+    }
+    return true;
+  }
+
   getDataAtual() {
     var data = new Date();
     var dia: number = data.getDate();
