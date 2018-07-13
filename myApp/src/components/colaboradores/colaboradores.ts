c50563105ec885eed4e9d0247d65dc64b89a9190
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2018-07-13 14:37:36 -0300

    nao consegue enviar notification para o firestore

--- myApp/src/components/colaboradores/colaboradores.ts
+++ myApp/src/components/colaboradores/colaboradores.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {AngularFirestore} from 'angularfire2/firestore';
+import {AngularFireAuth} from 'angularfire2/auth';
+import { UrlSerializer } from 'ionic-angular/umd';
+import { makePropDecorator } from '@angular/core/src/util/decorators';
 
 /**
  * Generated class for the ColaboradoresComponent component.
@@ -14,8 +18,47 @@ export class ColaboradoresComponent {
 
 
 
-  constructor() {
- 
+  constructor(public afs: AngularFirestore, private afAuth: AngularFireAuth) {
+
   }
+
+  
+   ngOnInit(){
+     this.sendCollaborationRequest('pedro.braga@ccc.ufcg.edu.br', 'nomeDoProjeto', 'idDoProjeto');
+  }
+
+  
+
+    
+
+  //Envia notificacao de colaboracao para um usuario do sistema
+  public sendCollaborationRequest(userEmail, projectName, projectId){
+    var userRequestedId = 'idDoUsuarioAReceberNotification'
+    const notificationsRef = this.afs.firestore.collection('notifications');
+    const usersRef = this.afs.firestore.collection('users');
+
+    
+    //Codigo que busca usuario para atribuir o id a notification
+    usersRef.where('email', '==', userEmail).get()
+    .then(snap => userRequestedId = snap.docs[0].id)
+    .catch(err => userRequestedId = 'idDoDocumento'); 
+    
+    const notification = {
+      message: 'Você foi convidado para participar do projeto ' + projectName,
+      projectId,
+      seen: 'true',
+      type: 'novoprojeto',
+      userId: userRequestedId
+    };
+
+    
+
+    notificationsRef.add(notification)
+    .then(response => console.log("Notification sent! " + response))
+    .catch(err => console.log('Error while sending notification! id do usuario: ' + 
+    this.afAuth.auth.currentUser.uid + " err: " + err));
+    
+
+} 
 
 }
