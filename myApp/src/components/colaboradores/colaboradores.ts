d8cf35b986a804514de049573b45641d8c118272
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2018-07-14 11:42:00 -0300

    Informa se o convite foi enviado ou nao

--- myApp/src/components/colaboradores/colaboradores.ts
+++ myApp/src/components/colaboradores/colaboradores.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
 import {AngularFireAuth} from 'angularfire2/auth';
+import {ToastController} from 'ionic-angular'
 
 
 /**
@@ -17,13 +18,13 @@ export class ColaboradoresComponent {
 
   
 
-  constructor(public afs: AngularFirestore, private afAuth: AngularFireAuth) {
+  constructor(public toastCtrl: ToastController, public afs: AngularFirestore, private afAuth: AngularFireAuth) {
 
   }
 
   
-   ngOnInit(){
-     
+   ngOnInit(){ //Como ainda não temos o botão, testando aqui
+     this.sendCollaborationRequest('pedro.braga@ccc.ufcg.edu.br', 'VLvjoxho1cSLW7859WBv')
   }
 
   
@@ -31,23 +32,19 @@ export class ColaboradoresComponent {
     
 
   public async sendCollaborationRequest(userEmail, projectId){
-    var userRequestedId;
-    var projectName
+    var userRequestedId, projectName;
     const notificationsRef = this.afs.collection('notifications')
     const usersRef = this.afs.firestore.collection('users');
     const projectsRef = this.afs.firestore.collection('projects');
+    var toast = this.toastCtrl.create({});
 
-    
     await projectsRef.doc(projectId).get()
     .then(snap => projectName = snap.data().projectName)
     .catch(err => console.log('Error while finding project name!'))
 
-
-
-    //Busca usuario para atribuir o id a notification
      await usersRef.where('email', '==', userEmail).get()
     .then(snap => userRequestedId = snap.docs[0].id)
-    .catch(err => console.log('Error while finding userRequestedId!')); 
+    .catch(err => toast.setMessage('Usuário não encontrado, verifique o e-mail e tente novamente')); 
 
     const notification = {
       message: 'Você foi convidado para participar do projeto ' + projectName,
@@ -56,15 +53,12 @@ export class ColaboradoresComponent {
       type: 'convitecolaborador',
       userId: userRequestedId
     };
-
     
-
-    notificationsRef.add(notification)
-    .then(response => console.log("Notification sent! " + response))
-    .catch(err => console.log('Error while sending notification! id do usuario: ' + 
-    this.afAuth.auth.currentUser.uid + " err: " + err));
-    
-
-} 
-
+    if(userRequestedId != undefined){
+      await notificationsRef.add(notification)
+      .then(response => toast.setMessage('Convite enviado com sucesso'))
+      .catch(err => toast.setMessage('Erro ao enviar o convite. Tente novamente'));
+    }
+    toast.setDuration(3000).setPosition('top').present();
+  } 
 }
